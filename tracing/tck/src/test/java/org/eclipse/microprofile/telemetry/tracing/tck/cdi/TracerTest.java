902d1e7da5fca8028d413398873b180e74a7f056
brunobat <brunobat@gmail.com>
2022-05-23 15:43:32 +0100

    #8 - Port code from smallrye opentelemetry project - further changes needed

---
+++ tracing/tck/src/test/java/org/eclipse/microprofile/telemetry/tracing/tck/cdi/TracerTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016-2022 Contributors to the Eclipse Foundation
+ *
+ *  See the NOTICE file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.eclipse.microprofile.telemetry.tracing.tck.cdi;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.opentelemetry.api.trace.Tracer;
+
+@ExtendWith(ArquillianExtension.class)
+class TracerTest {
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class);
+    }
+
+    @Inject
+    TracerBean tracerBean;
+
+    @Test
+    void tracer() {
+        assertNotNull(tracerBean.getTracer());
+    }
+
+    @ApplicationScoped
+    public static class TracerBean {
+        @Inject
+        Tracer tracer;
+
+        public Tracer getTracer() {
+            return tracer;
+        }
+    }
+}
