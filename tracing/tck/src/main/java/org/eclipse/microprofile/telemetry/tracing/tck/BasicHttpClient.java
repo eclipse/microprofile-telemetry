d6b6ff08c2d70933c9b35b9fd1f9a11b49837f50
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2023-07-28 09:13:35 +0100

    Update tracing/tck/src/main/java/org/eclipse/microprofile/telemetry/tracing/tck/BasicHttpClient.java
    Co-authored-by: Andrew Rouse <anrouse@uk.ibm.com>

--- tracing/tck/src/main/java/org/eclipse/microprofile/telemetry/tracing/tck/BasicHttpClient.java
+++ tracing/tck/src/main/java/org/eclipse/microprofile/telemetry/tracing/tck/BasicHttpClient.java
@@ -90,15 +90,13 @@ public class BasicHttpClient {
         try {
             URL spanUrl = baseUri.resolve(path).toURL();
             HttpURLConnection connection = (HttpURLConnection) spanUrl.openConnection();
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 String inputLine;
-                StringBuffer response = new StringBuffer();
+                StringBuilder response = new StringBuilder();
 
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
-                in.close();
                 return response.toString();
             } catch (Exception e) {
                 throw new RuntimeException("Exception retrieving " + spanUrl, e);
