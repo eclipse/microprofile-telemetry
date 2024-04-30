76708127e037d2797cf808094f0a6f5bef954be3
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2024-04-30 15:42:37 +0100

    Update logs TCK

--- tck/logs/src/main/java/org/eclipse/microprofile/telemetry/logs/tck/application/LogAppenderTest.java
+++ tck/logs/src/main/java/org/eclipse/microprofile/telemetry/logs/tck/application/LogAppenderTest.java
@@ -20,6 +20,7 @@
 
 package org.eclipse.microprofile.telemetry.logs.tck.application;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.eclipse.microprofile.telemetry.logs.tck.TestLibraries;
@@ -31,6 +32,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -43,14 +45,25 @@ import jakarta.inject.Inject;
 public class LogAppenderTest extends Arquillian {
     @Deployment
     public static WebArchive createDeployment() {
+        File[] slf4jFiles = Maven.resolver().resolve("org.slf4j:jul-to-slf4j:2.0.13").withTransitivity().asFile();
+        File[] telemetryLogbackFiles = Maven.resolver()
+                .resolve("io.opentelemetry.instrumentation:opentelemetry-logback-appender-1.0:2.2.0-alpha")
+                .withTransitivity().asFile();
+        File[] logbackFiles =
+                Maven.resolver().resolve("ch.qos.logback:logback-classic:1.5.5").withTransitivity().asFile();
+
         return ShrinkWrap.create(WebArchive.class)
                 .addClasses(InMemoryLogRecordExporter.class, InMemoryLogRecordExporterProvider.class)
                 .addAsLibrary(TestLibraries.AWAITILITY_LIB)
+                .addAsLibraries(slf4jFiles)
+                .addAsLibraries(telemetryLogbackFiles)
+                .addAsLibraries(logbackFiles)
                 .addAsServiceProvider(ConfigurableLogRecordExporterProvider.class,
                         InMemoryLogRecordExporterProvider.class)
                 .addAsResource(new StringAsset(
-                        "otel.sdk.disabled=false\notel.metrics.exporter=\notel.traces.exporter=none\notel.logs.exporter=in-memory"),
+                        "otel.sdk.disabled=false\notel.metrics.exporter=none\notel.traces.exporter=none\notel.logs.exporter=in-memory\notel.blrp.max.export.batch.size=1\notel.blrp.schedule.delay=30"),
                         "META-INF/microprofile-config.properties")
+                .addAsResource("logback.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
@@ -60,7 +73,7 @@ public class LogAppenderTest extends Arquillian {
     @Inject
     private InMemoryLogRecordExporter memoryExporter;
 
-    private static final java.util.logging.Logger julLogger = Logger.getLogger("jul-logger");
+    private static final Logger julLogger = Logger.getLogger("jul-logger");
 
     @Test
     void julTest() throws InterruptedException {
@@ -71,6 +84,8 @@ public class LogAppenderTest extends Arquillian {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
         julLogger.info("A JUL log message");
-        Assert.assertEquals(memoryExporter.getFinishedLogRecordItems(1).get(0).getBody(), "A JUL log message");
+
+        Assert.assertTrue(
+                memoryExporter.getFinishedLogRecordItems(1).get(0).getBody().toString().contains("A JUL log message"));
     }
 }
