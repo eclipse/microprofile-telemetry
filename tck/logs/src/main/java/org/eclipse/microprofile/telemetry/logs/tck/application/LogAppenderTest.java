6244f7615fc24704cda7b6918d4f0b7f4b36d9bf
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2024-06-11 22:20:32 +0100

    Update TCK (#178)
    * Add logs TCK
    
    * Update logs TCK
    
    * test jul

--- tck/logs/src/main/java/org/eclipse/microprofile/telemetry/logs/tck/application/LogAppenderTest.java
+++ tck/logs/src/main/java/org/eclipse/microprofile/telemetry/logs/tck/application/LogAppenderTest.java
@@ -23,33 +23,24 @@ package org.eclipse.microprofile.telemetry.logs.tck.application;
 import java.util.logging.Logger;
 
 import org.eclipse.microprofile.telemetry.logs.tck.TestLibraries;
-import org.eclipse.microprofile.telemetry.logs.tck.exporter.InMemoryLogRecordExporter;
-import org.eclipse.microprofile.telemetry.logs.tck.exporter.InMemoryLogRecordExporterProvider;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.slf4j.bridge.SLF4JBridgeHandler;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
-import io.opentelemetry.sdk.autoconfigure.spi.logs.ConfigurableLogRecordExporterProvider;
 import jakarta.inject.Inject;
 
 public class LogAppenderTest extends Arquillian {
     @Deployment
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class)
-                .addClasses(InMemoryLogRecordExporter.class, InMemoryLogRecordExporterProvider.class)
                 .addAsLibrary(TestLibraries.AWAITILITY_LIB)
-                .addAsServiceProvider(ConfigurableLogRecordExporterProvider.class,
-                        InMemoryLogRecordExporterProvider.class)
                 .addAsResource(new StringAsset(
-                        "otel.sdk.disabled=false\notel.metrics.exporter=none\notel.traces.exporter=none\notel.logs.exporter=otlp"),
+                        "otel.sdk.disabled=false\notel.metrics.exporter=none\notel.traces.exporter=none\notel.logs.exporter=logging\notel.service.name=openliberty"),
                         "META-INF/microprofile-config.properties")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
@@ -57,21 +48,15 @@ public class LogAppenderTest extends Arquillian {
     @Inject
     private OpenTelemetry openTelemetry;
 
-    @Inject
-    private InMemoryLogRecordExporter memoryExporter;
-
-    private static final java.util.logging.Logger julLogger = Logger.getLogger("jul-logger");
+    private static final Logger julLogger = Logger.getLogger("jul-logger");
 
     @Test
     void julTest() throws InterruptedException {
-        // Install OpenTelemetry in logback appender
-        OpenTelemetryAppender.install(openTelemetry);
 
-        // Route JUL logs to slf4j
-        SLF4JBridgeHandler.removeHandlersForRootLogger();
-        SLF4JBridgeHandler.install();
         julLogger.info("A JUL log message");
-        System.out.println(openTelemetry);
-        Assert.assertEquals(memoryExporter.getFinishedLogRecordItems(1).get(0).getBody(), "A JUL log message");
+        julLogger.info("A JUL log message");
+        julLogger.info("A JUL log message");
+        julLogger.info("A JUL log message");
+        Thread.sleep(10000);
     }
 }
