73cd4a2c73fa09256dfca4f30b2d405b08eec1b1
Benjamin Confino <benjamic@uk.ibm.com>
2024-07-04 08:37:44 +0100

    fix comment (#215)

--- tck/tracing/src/main/java/org/eclipse/microprofile/telemetry/tracing/tck/porting/api/ConfigurationAccessor.java
+++ tck/tracing/src/main/java/org/eclipse/microprofile/telemetry/tracing/tck/porting/api/ConfigurationAccessor.java
@@ -1,22 +1,19 @@
 /*
- * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ * Copyright 2010, Red Hat, Inc., and individual contributors
  *
- *  See the NOTICE file(s) distributed with this work for additional
- *  information regarding copyright ownership.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  You may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
+//This code comes from
+//https://github.com/jakartaee/cdi-tck/blob/master/impl/src/main/java/org/jboss/cdi/tck/impl/ConfigurationImpl.java
+//with minor changes. Full credit to the original authors.
 package org.eclipse.microprofile.telemetry.tracing.tck.porting.api;
 
 import org.eclipse.microprofile.telemetry.tracing.tck.porting.PropertiesBasedConfigurationBuilder;
@@ -30,7 +27,7 @@ public class ConfigurationAccessor {
     /**
      * @param deploymentPhase
      *            Deployment phase (building test archive) initialization includes deployment specific properties
-     * @return current JSR 365 TCK configuration
+     * @return current mp telemetry TCK configuration
      */
     public static Configuration get(boolean deploymentPhase) {
 
@@ -45,7 +42,7 @@ public class ConfigurationAccessor {
     }
 
     /**
-     * @return current JSR 365 TCK configuration
+     * @return current mp telemetry TCK configuration
      */
     public static Configuration get() {
         return get(false);
