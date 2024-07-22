8106fb89ba0b7e17c5ef0e035e156f75a022cf27
Benjamin Confino <benjamic@uk.ibm.com>
2024-07-22 17:04:54 +0100

    remove todo

--- tck/metrics/src/main/java/org/eclipse/microprofile/telemetry/metrics/tck/jvm/JvmGarbageCollectionTest.java
+++ tck/metrics/src/main/java/org/eclipse/microprofile/telemetry/metrics/tck/jvm/JvmGarbageCollectionTest.java
@@ -84,15 +84,14 @@ public class JvmGarbageCollectionTest extends Arquillian {
                 // The JIT doesn't optimise it away
                 String alsoIgnored = fromWeakRef.strip();
             }
-            
+
             try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        
+
         long stopTime = System.nanoTime();
         long seconds = TimeUnit.SECONDS.toSeconds(stopTime - startTime);
 
